16513bc34b126ebcb49d763b443cc1e59a82d1db
Ee Kin <dickson_chin93@hotmail.com>
2018-02-24 21:59:02 +0800

    Add files via upload

---
+++ get_data.sh
@@ -0,0 +1,3 @@
+curl -L -o final_train.csv "https://drive.google.com/uc?export=download&id=19WFLluCA0YNXLOAOVwN4iKul83-AXpPv"
+curl -L -o test.csv "https://drive.google.com/uc?export=download&id=19WFLluCA0YNXLOAOVwN4iKul83-AXpPv"
+
