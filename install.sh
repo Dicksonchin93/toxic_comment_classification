34f93da89aece7e89ebf874ba9b45fbe2b6eba01
Ee Kin <dickson_chin93@hotmail.com>
2018-02-25 15:05:54 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 sudo apt-get install cython htop \
-wget https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.en.zip
+wget https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.en.zip \ 
+git clone https://github.com/facebookresearch/fastText.git \ 
+cd fastText \
+pip install .
